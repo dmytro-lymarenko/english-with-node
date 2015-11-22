1fcb075acab72528a5ad946800e4e9cc391a1564
Dmytro <dmytro.lymarenko@gmail.com>
2015-11-22 15:36:56 +0200

    fix settings

--- server/run.js
+++ server/run.js
@@ -1,6 +1,7 @@
 var server = require('http').createServer();
 var Teacher = require('./modules/teacher');
-var Settings = new require('./modules/settings');
+var Settings = require('./modules/settings');
+
 var settings = new Settings();
 
 var teacher = new Teacher(settings.getSettings());
